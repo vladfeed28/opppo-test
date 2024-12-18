b1b6617f4553bb7d9ae05974fcc1226d7c2f8176
vladfeed28 <62261137+vladfeed28@users.noreply.github.com>
2024-12-18 12:39:19 +0700

    add feature_fast without my_pow

--- main.cpp
+++ main.cpp
@@ -1,14 +1,6 @@
   #include <iostream>
   #include <cmath>
 
-  double my_pow(double a, unsigned int b) {
-    double result = 1.0;
-    for (unsigned int i = 0; i < b; ++i) {
-      result *= a;
-    }
-    return result;
-  }
-
   int main() {
     std::cout << std::pow(3.0, 4) << std::endl;
     return 0;
