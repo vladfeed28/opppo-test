0cd0cadd2554c886ccc182d4a9076404a49d2191
vladfeed28 <62261137+vladfeed28@users.noreply.github.com>
2024-12-18 12:44:47 +0700

    fixed my_pow

--- main.cpp
+++ main.cpp
@@ -1,14 +1,20 @@
   #include <iostream>
+  #include <cmath> // для std::pow
 
-  double my_pow(double a, unsigned int b) {
-    double result = 1.0;
-    for (unsigned int i = 0; i < b; ++i) {
-      result *= a;
+  double my_pow(double a, int b) {
+    if (b >= 0) {
+        double result = 1.0;
+        for (int i = 0; i < b; ++i) {
+            result *= a;
+        }
+        return result;
+    } else {
+        return 1.0 / my_pow(a, -b); 
     }
-    return result;
   }
 
   int main() {
     std::cout << my_pow(3.0, 4) << std::endl;
+    std::cout << my_pow(3.0, -4) << std::endl;
     return 0;
   }
